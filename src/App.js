b4896a09b55c51319f0fd2f8b04bdb7b74666864
Cody Marshall <codymstuntsx@gmail.com>
2022-09-26 13:56:53 -0700

    get call to display games

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import GameList from "./components/GameList";
+import Header from "./components/Header";
+
+const axios = require('axios');
+// 317612d4689e479aa46dc75b7e45ec7d API KEY
 
 function App() {
+const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.rawg.io/api/games?key=317612d4689e479aa46dc75b7e45ec7d")
+    .then(res => {
+      setGames(res.data.results);
+      console.log(res.data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
+  
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <h1 className="game-section">Current Games</h1>
+      <GameList games={games}/>
+    </div>
   );
 }
 
