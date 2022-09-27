a65087ce8eefadf2df041519865f7527f086f143
Cody Marshall <codymstuntsx@gmail.com>
2022-09-27 14:53:05 -0700

    refactored app to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import GameList from "../components/GameList";
+import Loading from "../components/Loading";
+import Pagination from "../components/Pagination";
 
-function Home() {
+
+function Home({getNextPage, getPreviousPage, prevPage, games, isLoading, handleSelectedGame}) {
+  
   return (
-    <div>Home</div>
+    <div>
+      <div className="container d-flex justify-content-center flex-column">
+        <Pagination
+          getPreviousPage={getPreviousPage}
+          getNextPage={getNextPage}
+          prevPage={prevPage}
+        />
+        <h1 className="game-section">Current Games</h1>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <GameList games={games} handleSelectedGame={handleSelectedGame} />
+            <Pagination
+              getPreviousPage={getPreviousPage}
+              getNextPage={getNextPage}
+              prevPage={prevPage}
+            />
+          </>
+        )}
+      </div>
+    </div>
   )
 }
 
