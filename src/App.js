4b87cef196f9e5b040138df5649bca02d5252af2
Cody Marshall <codymstuntsx@gmail.com>
2022-09-27 13:52:06 -0700

    axios get requests and ifLoading check

--- src/App.js
+++ src/App.js
@@ -1,31 +1,76 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import GameInfo from "./components/GameInfo";
 import GameList from "./components/GameList";
 import Header from "./components/Header";
+import Loading from "./components/Loading";
+import Pagination from "./components/Pagination";
 
-const axios = require('axios');
+const axios = require("axios");
 // https://rawg.io/
 // 317612d4689e479aa46dc75b7e45ec7d API KEY
 
 function App() {
-const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
+  const [currentPage, setCurrentPage] = useState(
+    "https://api.rawg.io/api/games?key=317612d4689e479aa46dc75b7e45ec7d"
+  );
+  const [prevPage, setPrevPage] = useState(null);
+  const [nextPage, setNextPage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedGame, setSelectedGame] = useState();
 
   useEffect(() => {
-    axios.get("https://api.rawg.io/api/games?key=317612d4689e479aa46dc75b7e45ec7d")
-    .then(res => {
-      setGames(res.data.results);
-      console.log(res.data.results);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }, []);
-  
+    setIsLoading(true);
+    axios
+      .get(`${currentPage}`)
+      .then((res) => {
+        setGames(res.data.results);
+        setNextPage(res.data.next);
+        setPrevPage(res.data.previous);
+        setTimeout(() => setIsLoading(false), 250);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [currentPage]);
+
+  const getNextPage = () => {
+    setCurrentPage(nextPage);
+  };
+
+  const getPreviousPage = () => {
+    setCurrentPage(prevPage);
+  };
+
+  const handleSelectedGame = (game) => {
+    setSelectedGame(game);
+  };
+
   return (
-    <div className="container d-flex justify-content-center flex-column">
-      <Header />
-      <h1 className="game-section">Current Games</h1>
-      <GameList games={games}/>
+    <div>
+      <div className="container d-flex justify-content-center flex-column">
+        <Header />
+        <Pagination
+          getPreviousPage={getPreviousPage}
+          getNextPage={getNextPage}
+          prevPage={prevPage}
+        />
+        <h1 className="game-section">Current Games</h1>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <GameList games={games} handleSelectedGame={handleSelectedGame} />{" "}
+            <Pagination
+              getPreviousPage={getPreviousPage}
+              getNextPage={getNextPage}
+              prevPage={prevPage}
+            />
+          </>
+        )}
+      </div>
+      <GameInfo selectedGame={selectedGame} />
     </div>
   );
 }
