0171643947df8429407e0e30328c2a9d296fa294
Cody Marshall <codymstuntsx@gmail.com>
2022-09-27 13:52:44 -0700

    added name, screenshots and general info

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "../css/gameinfo.css";
 
+const xbox = require("../icons/Xbox.png");
+const pc = require("../icons/PC.png");
+const playstation = require("../icons/PlayStation.png");
+const android = require("../icons/Android.png");
+const ios = require("../icons/iOS.png");
+const linux = require("../icons/Linux.png");
+const apple = require("../icons/Apple.png");
+const nintendo = require("../icons/Nintendo.png");
+
 function GameInfo({ selectedGame }) {
   if (selectedGame !== undefined) {
     return (
@@ -10,8 +19,69 @@ function GameInfo({ selectedGame }) {
           src={selectedGame["background_image"]}
           alt=""
         />
-
         <div className="selected-game-name">{selectedGame.name}</div>
+        <div className="selected-game-general">
+          <div className="esrb-rating">{selectedGame["esrb_rating"].name}</div>
+          {selectedGame.genres.map((genre) => (
+            <div className="genre" key={genre.id}>
+              {genre.name}
+            </div>
+          ))}
+        </div>
+        <div className="screenshot-container">
+          {selectedGame["short_screenshots"].map((screenshot) => (
+            <img
+              src={screenshot.image}
+              className="selected-game-screenshot"
+              key={screenshot.id}
+              alt=""
+            />
+          ))}
+        </div>
+        <div className="selected-game-platform-container">
+          {selectedGame["parent_platforms"].map((platform) => (
+            <img
+              key={platform.platform.id}
+              className="selected-game-platform-icon"
+              src={
+                platform.platform.name === "Xbox"
+                  ? xbox
+                  : platform.platform.name === "PlayStation"
+                  ? playstation
+                  : platform.platform.name === "PC"
+                  ? pc
+                  : platform.platform.name === "Android"
+                  ? android
+                  : platform.platform.name === "iOS"
+                  ? ios
+                  : platform.platform.name === "Linux"
+                  ? linux
+                  : platform.platform.name === "Apple Macintosh"
+                  ? apple
+                  : platform.platform.name === "Nintendo"
+                  ? nintendo
+                  : "No Image"
+              }
+              alt="platform"
+            ></img>
+          ))}
+        </div>
+        <div className="selected-game-main-info">
+            <div>Release Data:</div>
+            <div>{selectedGame.released}</div>
+          <div>Platforms:</div>
+          <div>
+            {selectedGame.platforms.map((platform) => (
+              <span className="selected-game-platform" key={platform.id}>{platform.platform.name}</span>
+            ))}
+          </div>
+          <div>Tags:</div>
+          <div>
+            {selectedGame.tags.map((tag) => (
+              <span className="selected-game-tag" key={tag.id}>{tag.name}</span>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
