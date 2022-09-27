16615e959f1aeaf868eb47bac8cf177797584664
Cody Marshall <codymstuntsx@gmail.com>
2022-09-27 14:52:49 -0700

    wrapped container with router link

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../css/gamelist.css";
 
 const xbox = require("../icons/Xbox.png");
@@ -15,57 +16,70 @@ function GameList({ games, handleSelectedGame }) {
     <div className="container d-flex justify-content-center">
       <div className="row d-flex justify-content-center">
         {games.map((game) => (
-          <div className="col-sm-10 col-md-5 col-lg-3 col-xl-2 game-container" key={game.id} onClick={() => handleSelectedGame(game)}>
-            <div className="img-container">
-              <img src={game["background_image"]} className="game-img" alt="" />
+          <Link
+            to={`info`}
+            key={game.id}
+            className="link-style"
+            onClick={() => handleSelectedGame(game)}
+          >
+            <div className="col-sm-10 col-md-5 col-lg-3 col-xl-2 game-container">
+              <div className="img-container">
+                <img
+                  src={game["background_image"]}
+                  className="game-img"
+                  alt=""
+                />
+              </div>
+              <div className="game-info">
+                <div>
+                  {game["parent_platforms"].map((platform) => (
+                    <img
+                      key={platform.platform.id}
+                      className="platform-icon"
+                      src={
+                        platform.platform.name === "Xbox"
+                          ? xbox
+                          : platform.platform.name === "PlayStation"
+                          ? playstation
+                          : platform.platform.name === "PC"
+                          ? pc
+                          : platform.platform.name === "Android"
+                          ? android
+                          : platform.platform.name === "iOS"
+                          ? ios
+                          : platform.platform.name === "Linux"
+                          ? linux
+                          : platform.platform.name === "Apple Macintosh"
+                          ? apple
+                          : platform.platform.name === "Nintendo"
+                          ? nintendo
+                          : "No Image"
+                      }
+                      alt="platform"
+                    ></img>
+                  ))}
+                </div>
+                <div>
+                  <span className="game-info-name">{game.name}</span>
+                </div>
+                <div className="game-info-rating mt-3">{game.metacritic}</div>
+              </div>
+              <div className="game-info-hover" id="game-info-hover">
+                <div className="game-info-hover-divider">
+                  <span className="game-info-hover-title">Release Date:</span>
+                  <span>{game.released}</span>
+                </div>
+                <div className="game-info-hover-divider">
+                  <span className="game-info-hover-title">Genre:</span>
+                  {game.genres.map((genre) => (
+                    <span className="p-1" key={genre.id}>
+                      {genre.name}
+                    </span>
+                  ))}
+                </div>
+              </div>
             </div>
-            <div className="game-info">
-              <div>
-                {game["parent_platforms"].map((platform) => (
-                  <img
-                    key={platform.platform.id}
-                    className="platform-icon"
-                    src={
-                      platform.platform.name === "Xbox"
-                        ? xbox
-                        : platform.platform.name === "PlayStation"
-                        ? playstation
-                        : platform.platform.name === "PC"
-                        ? pc
-                        : platform.platform.name === "Android"
-                        ? android
-                        : platform.platform.name === "iOS"
-                        ? ios
-                        : platform.platform.name === "Linux"
-                        ? linux
-                        : platform.platform.name === "Apple Macintosh"
-                        ? apple
-                        : platform.platform.name === "Nintendo"
-                        ? nintendo
-                        : "No Image"
-                    }
-                    alt="platform"
-                  ></img>
-                ))}
-              </div>
-              <div>
-                <span className="game-info-name">{game.name}</span>
-              </div>
-              <div className="game-info-rating mt-3">{game.metacritic}</div>
-            </div>
-            <div className="game-info-hover" id="game-info-hover">
-              <div className="game-info-hover-divider">
-                <span className="game-info-hover-title">Release Date:</span>
-                <span>{game.released}</span>
-              </div>
-              <div className="game-info-hover-divider">
-                <span className="game-info-hover-title">Genre:</span>
-                {game.genres.map((genre) => (
-                  <span className="p-1" key={genre.id}>{genre.name}</span>
-                ))}
-              </div>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
