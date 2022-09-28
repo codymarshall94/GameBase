1ee103494f7021898beaf4e4198c08ec3a839a91
Cody Marshall <codymstuntsx@gmail.com>
2022-09-28 13:29:27 -0700

    main info text color and text wrap

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -67,15 +67,15 @@ function GameInfo({ selectedGame }) {
           ))}
         </div>
         <div className="selected-game-main-info">
-            <div>Release Data:</div>
+            <div className="main-info-title">Release Data:</div>
             <div>{selectedGame.released}</div>
-          <div>Platforms:</div>
+          <div className="main-info-title">Platforms:</div>
           <div>
             {selectedGame.platforms.map((platform) => (
               <span className="selected-game-platform" key={platform.platform.id} >{platform.platform.name}</span>
             ))}
           </div>
-          <div>Tags:</div>
+          <div className="main-info-title">Tags:</div>
           <div>
             {selectedGame.tags.map((tag) => (
               <span className="selected-game-tag" key={tag.id} >{tag.name}</span>
